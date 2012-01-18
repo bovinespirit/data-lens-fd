d44f313093d2426868fe6f98286103dcd17f170a
Matt West <git@matthewwest.co.uk>
2012-01-18 21:23:56 +0000

    Change return types to () instead of result.
    Update documentation

--- Data/Lens.hs
+++ Data/Lens.hs
@@ -4,15 +4,15 @@ module Data.Lens
   ( module Data.Lens.Common
   -- * State API
   , access         -- getter -- :: MonadState a m => Lens a b -> m b
-  , (~=), (!=)     -- setter -- :: MonadState a m => Lens a b -> b -> m b
-  , (%=), (!%=)    -- modify -- :: MonadState a m => Lens a b -> (b -> b) -> m b
+  , (~=), (!=)     -- setter -- :: MonadState a m => Lens a b -> b -> m ()
+  , (%=), (!%=)    -- modify -- :: MonadState a m => Lens a b -> (b -> b) -> m ()
   , (%%=), (!%%=)  -- modify -- :: MonadState a m => Lens a b -> (b -> (c, b)) -> m c
-  , (+=), (!+=)    -- modify -- :: (MonadState a m, Num b) => Lens a b -> b -> m b
-  , (-=), (!-=)    -- modify -- :: (MonadState a m, Num b) => Lens a b -> b -> m b
-  , (*=), (!*=)    -- modify -- :: (MonadState a m, Num b) => Lens a b -> b -> m b
-  , (//=), (!/=)   -- modify -- :: (MonadState a m, Fractional b) => Lens a b -> b -> m b
-  , (&&=), (!&&=)  -- modify -- :: MonadState a m => Lens a Bool -> Bool -> m Bool
-  , (||=), (!||=)  -- modify -- :: MonadState a m => Lens a Bool -> Bool -> m Bool
+  , (+=), (!+=)    -- modify -- :: (MonadState a m, Num b) => Lens a b -> b -> m ()
+  , (-=), (!-=)    -- modify -- :: (MonadState a m, Num b) => Lens a b -> b -> m ()
+  , (*=), (!*=)    -- modify -- :: (MonadState a m, Num b) => Lens a b -> b -> m ()
+  , (//=), (!/=)   -- modify -- :: (MonadState a m, Fractional b) => Lens a b -> b -> m ()
+  , (&&=), (!&&=)  -- modify -- :: MonadState a m => Lens a Bool -> Bool -> m ()
+  , (||=), (!||=)  -- modify -- :: MonadState a m => Lens a Bool -> Bool -> m ()
   , focus
   ) where
 
@@ -35,40 +35,38 @@ infixr 4 ~=, !=
 
 -- | Set a value using a lens into state.
 --
--- >> new_value <- field_name ~= value
-(~=) :: MonadState a m => Lens a b -> b -> m b
+-- >> field_name ~= value
+(~=) :: MonadState a m => Lens a b -> b -> m ()
 Lens f ~= b = do
   modify (peek b . f)
-  return b
+
 -- | Set a value using a lens into state strictly.
 --
--- >> new_value <- field_name !~= value
-(!=) :: MonadState a m => Lens a b -> b -> m b
+-- >> field_name !~= value
+(!=) :: MonadState a m => Lens a b -> b -> m ()
 Lens f != b = do
   StoreT (Identity h) _ <- gets f
   put (h $! b)
-  return b
 
 infixr 4 %=, !%=
 
 -- | Infix modification of a value through a lens into state.
 --
--- >> new_value <- field_name %= function
-(%=)  :: MonadState a m => Lens a b -> (b -> b) -> m b
+-- >> field_name %= function
+(%=)  :: MonadState a m => Lens a b -> (b -> b) -> m ()
 Lens f %= g = do
   StoreT (Identity h) b <- gets f
   let b' = g b
   put (h b')
-  return b'
+
 -- | Strict Infix modification of a value through a lens into state.
 --
 -- >> new_value <- field_name !%= function
-(!%=) :: MonadState a m => Lens a b -> (b -> b) -> m b
+(!%=) :: MonadState a m => Lens a b -> (b -> b) -> m ()
 Lens f !%= g = do
   StoreT (Identity h) b <- gets f
   let b' = g b
   b' `seq` put (h b')
-  return b'
 
 infixr 4 %%=, !%%=
 
@@ -98,48 +96,48 @@ infixr 4 +=, !+=, -=, !-=, *=, !*=
 -- | Perform addition on a value through a lens into state.
 --
 -- >> do
--- >>   field ~= 5
--- >>   new_value <- field += 4   -- Add 4 to the value of field
+-- >>   field ~= 5   -- Set the value of field to 5
+-- >>   field += 4   -- Add 4 to the value of field
 -- >>   state_value <- access field
--- >>   -- new_value == state_value == 9
-(+=) :: (MonadState a m, Num b) => Lens a b -> b -> m b
+-- >>   -- state_value == 9
+(+=) :: (MonadState a m, Num b) => Lens a b -> b -> m ()
 f += b = f %= (+ b)
 -- | Perform subtraction on a value through a lens into state.
-(-=) :: (MonadState a m, Num b) => Lens a b -> b -> m b
+(-=) :: (MonadState a m, Num b) => Lens a b -> b -> m ()
 f -= b = f %= subtract b
 -- |  Multiply a value by a value through a lens into state.
-(*=) :: (MonadState a m, Num b) => Lens a b -> b -> m b
+(*=) :: (MonadState a m, Num b) => Lens a b -> b -> m ()
 f *= b = f %= (* b)
 -- | Perform addition on a value through a lens into state strictly.
-(!+=) :: (MonadState a m, Num b) => Lens a b -> b -> m b
+(!+=) :: (MonadState a m, Num b) => Lens a b -> b -> m ()
 f !+= b = f !%= (+ b)
 -- | Perform subtraction on a value through a lens into state strictly.
-(!-=) :: (MonadState a m, Num b) => Lens a b -> b -> m b
+(!-=) :: (MonadState a m, Num b) => Lens a b -> b -> m ()
 f !-= b = f !%= subtract b
 -- | Perform multiplication on a value through a lens into state strictly.
-(!*=) :: (MonadState a m, Num b) => Lens a b -> b -> m b
+(!*=) :: (MonadState a m, Num b) => Lens a b -> b -> m ()
 f !*= b = f !%= (* b)
 
 infixr 4 //=, !/=
 
 -- | Perform division on a value through a lens into state.
-(//=) :: (MonadState a m, Fractional b) => Lens a b -> b -> m b
+(//=) :: (MonadState a m, Fractional b) => Lens a b -> b -> m ()
 f //= b = f %= (/ b)
 -- | Perform division on a value through a lens into state strictly.
-(!/=) :: (MonadState a m, Fractional b) => Lens a b -> b -> m b
+(!/=) :: (MonadState a m, Fractional b) => Lens a b -> b -> m ()
 f !/= b = f !%= (/ b)
 
 infixr 4 &&=, !&&=, ||=, !||=
 
 -- | Perform a boolean @and@ operation on a value through a lens into state.
-(&&=) :: MonadState a m => Lens a Bool -> Bool -> m Bool
+(&&=) :: MonadState a m => Lens a Bool -> Bool -> m ()
 f &&= b = f %= (&& b)
 -- | Perform a boolean @or@ operation on a value through a lens into state.
-(||=) :: MonadState a m => Lens a Bool -> Bool -> m Bool
+(||=) :: MonadState a m => Lens a Bool -> Bool -> m ()
 f ||= b = f %= (|| b)
 -- | Perform a boolean @and@ operation on a value through a lens into state strictly.
-(!&&=) :: MonadState a m => Lens a Bool -> Bool -> m Bool
+(!&&=) :: MonadState a m => Lens a Bool -> Bool -> m ()
 f !&&= b = f !%= (&& b)
 -- | Perform a boolean @or@ operation on a value through a lens into state strictly.
-(!||=) :: MonadState a m => Lens a Bool -> Bool -> m Bool
+(!||=) :: MonadState a m => Lens a Bool -> Bool -> m ()
 f !||= b = f !%= (|| b)
